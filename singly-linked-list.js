ff28efabf7dd27049e06ff3b240191202c88c02d
Sarah French <sarah@sarahfrench.co.uk>
2019-06-22 16:27:36 +0100

    Can delete a node. Need to update so indices aren't stored on node objs

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -53,13 +53,47 @@ class List {
       return this.findNodeByIndex(index, node.nextNode)
     }
   }
+
+  changeNextNodeAtIndex(index, newNextNode, node){
+    node = (typeof node !== 'undefined') ?  node : this.head; //if second argument not present assume starting from head of list object
+    newNextNode = (newNextNode instanceof Node) ?  newNextNode : new Node (newNextNodeindex+1);
+    if(node.index == index){
+      node.nextNode = newNextNode;
+      node.nextNode.index = index += 1
+    } else if (node.nextNode == 0){
+      return undefined;
+    } else {
+      this.changeNextNodeAtIndex(index, newNextNode, node.nextNode)
+    }
+  }
+
+  deleteNodeByValue(value){
+    let nodeToDelete = this.findNodeByValue(value);
+    let downstreamNode = nodeToDelete.nextNode;
+    this.changeNextNodeAtIndex(nodeToDelete.index-1, downstreamNode);
+    this.updateIndices()
+  }
+
+  updateIndices(node){
+    node = (typeof node !== 'undefined') ?  node : this.head; //if second argument not present assume starting from head of list object
+    if (node.nextNode.index != node.index+1 && node.nextNode != 0){
+      node.nextNode.index = node.index+1;
+      this.updateIndices(node.nextNode);
+    } else if (node.nextNode != 0) {
+      this.updateIndices(node.nextNode);
+    } else {
+      this.length = node.index + 1
+    }
+  }
+
 }
 
 list = new List()
 list.addNode("yabba");
 list.addNode("dabba");
 list.addNode("doo");
-console.log("The string 'doo' is in:");
-console.log(list.findIndexOfValue("doo"));
-console.log("The string 'qwerty' is in:");
-console.log(list.findIndexOfValue("qwerty"));
+list.addNode("Flintstones innit");
+console.log(list);
+list.deleteNodeByValue("doo")
+list.deleteNodeByValue("dabba")
+console.log(list);
