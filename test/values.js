7d194c9d900ff898c0e6abad04af526648913d38
Sarah French <sarah@sarahfrench.co.uk>
2019-06-23 10:30:06 +0100

    Tests for retrieving nodes by value

--- test/values.js
+++ test/values.js
@@ -8,16 +8,47 @@ const { Node, List } = require('../singly-linked-list.js');
 
 
 describe('Getting array values', function () {
- it('Get array of values from a list of 3 items', function () {
+ it('Get array of 3 correct values from a list of 3 items', function () {
       // Arrange
       let expectedValues = [ 'yabba', 'dabba', 'doo' ]
-      //Act
-      list = new List()
+      let list = new List()
       list.addNode("yabba");
       list.addNode("dabba");
       list.addNode("doo");
+
+      //Act
       let measuedValues = list.values()
+
       //Assert
       expect(deepEqual(measuedValues, expectedValues)).to.equal(true)
     });
+  it('Returns first Node with a value present the list', function () {
+       // Arrange
+       let list = new List()
+       list.addNode("yabba");
+       list.addNode("dabba");
+       list.addNode("doo");
+       let node = new Node ("dabba");
+       node.nextNode = new Node ("doo");
+       node.index = 1;
+       //Act
+       let foundNode = list.findNodeByValue("dabba")
+       //Assert
+       expect(foundNode.value).to.equal(node.value)
+       expect(foundNode.index).to.equal(node.index)
+       expect(foundNode.nextNode.value).to.equal(node.nextNode.value)
+
+     });
+  it('Returns undefined when asked to retrieve a Node with a value not present in the list', function () {
+       // Arrange
+       let list = new List()
+       list.addNode("yabba");
+       list.addNode("dabba");
+       list.addNode("doo");
+
+       //Act
+       let foundNode = list.findNodeByValue("not in list")
+       //Assert
+       expect(foundNode).to.equal(undefined)
+     });
 });
